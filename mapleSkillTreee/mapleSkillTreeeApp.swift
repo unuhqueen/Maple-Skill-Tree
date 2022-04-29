d8f8a1349eb9fc36f627441fbfd695fa756cf8c4
Seungwon <csw98213@gmail.com>
2022-04-29 09:45:11 +0900

    Initial Commit

---
+++ mapleSkillTreee/mapleSkillTreeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapleSkillTreeeApp.swift
+//  mapleSkillTreee
+//
+//  Created by Seungwon Choi on 2022/04/29.
+//
+
+import SwiftUI
+
+@main
+struct mapleSkillTreeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
